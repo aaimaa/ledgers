6e446e3e1926b6ac0ca30495b4ec4f717121858c
Asharam Seervi <seerviasharam@gmail.com>
2020-09-03 21:29:28 +0530

    Ejected: Notarize

--- build/notarize.js
+++ build/notarize.js
@@ -1,17 +1,17 @@
-const { notarize } = require('electron-notarize');
+// const { notarize } = require('electron-notarize');
 
-exports.default = async context => {
-  const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
+// exports.default = async context => {
+//   const { electronPlatformName, appOutDir } = context;
+//   if (electronPlatformName !== 'darwin') {
+//     return;
+//   }
 
-  const appName = context.packager.appInfo.productFilename;
+//   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    appBundleId: 'com.aaimaa.ledgers',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_APP_PASSWORD
-  });
-};
+//   return await notarize({
+//     appBundleId: 'com.aaimaa.ledgers',
+//     appPath: `${appOutDir}/${appName}.app`,
+//     appleId: process.env.APPLE_ID,
+//     appleIdPassword: process.env.APPLE_APP_PASSWORD
+//   });
+// };
